b0efe010487214b4ff9ba80e39166a0b43b4a465
vincentprr <vincentprr@users.noreply.github.com>
2022-09-21 12:11:44 +0200

    feat: Setting up environment and adding the head of the site.

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <head>
+        <title>Grand Galop<?php echo (isset($page) ? " - ". $page : "")?></title>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="includes/css/style.css"/>
+        <script src="includes/js/main.js"></script>
+    </head>
+
+    <body>
