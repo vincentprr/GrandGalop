f428486a5fc4cc4913d900dd8518043456a81038
vincentprr <vincentprr@users.noreply.github.com>
2022-09-26 15:22:25 +0200

    feat: Adding the content of the header.

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,25 @@
     <head>
         <title>Grand Galop<?php echo (isset($page) ? " - ". $page : "")?></title>
         <meta charset="UTF-8"/>
+        <link rel="icon" type="image/png" href="includes/img/logo.png"/>
         <link rel="stylesheet" href="includes/css/style.css"/>
         <script src="includes/js/main.js"></script>
     </head>
 
     <body>
+        <header>
+            <div>
+                <img src="includes/img/logo.png" alt="logo"/>
+                <div>
+                    <h1>Grand Galop</h1>
+                    <h2>Écurie de loisir</h2>
+                </div>
+            </div>
+        </header>
+
+        <nav id="navigation_menu">
+            <p><a href="">Accueil</a></p>
+            <p><a href="">Accueil</a></p>
+            <p><a href="">Accueil</a></p>
+            <p><a href="">Accueil</a></p>
+        </nav>
