89700812eb818f1461cab47591fd9194f578b87a
Bastian <rapine.bastian@gmail.com>
2022-10-10 14:30:06 +0200

    feat: ajout des instances de la base de données dans 'insert.sql'

--- sql/insert.sql
+++ sql/insert.sql
@@ -13,3 +13,25 @@ insert into PONEYS (NomP, ChargeMax, dateNaissanceP, tailleP, imgP) values
 ('Silver Bullet', 70, '2017-03-27', 123, null),
 ('Hey! Ya!', 60, '2016-05-19', 150, null),
 ('Valkyrie', 90, '2017-01-01', 163, null);
+
+insert into TYPESACTIVITE (NomTa) values
+("Balade"),
+("cours"),
+("initiation");
+
+insert into ACTIVITES (NomA, DescriptionA, IdTa, MaxClients) values
+("Balade en foret", "petite en foret pour tout les ages", 1, 10),
+("cours de saut", "apprendre à faire des sout à cheval", 2, 5),
+("premiere fois", "initiation à l'équitation pour les débutants", 3, 6);
+
+insert into SORTIES (IdA, IdM, DateSortie) values
+(1, 2, '2022-11-10 10:30:01'),
+(2, 1, '2022-12-11 14:00:00'),
+(3, 1, '2023-03-20 09:15:00');
+
+insert into MONTER (IdS, IdC, IdP) values
+(1, 1, 3),
+(1, 2, 2),
+(2, 3, 1),
+(2, 5, 3),
+(3, 4, 1);
