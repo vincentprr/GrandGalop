bb4c7f6cac98189f7c4470dcc26d0179b76ec25a
vincentprr <vincentprr@users.noreply.github.com>
2022-11-22 15:25:57 +0100

    chore: Changing from PHP to python.

---
+++ src/core/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from os import urandom
+from .constant import DATABASE_HOST, DATABASE_NAME, DATABASE_PASS, DATABASE_PROVIDER, DATABASE_USER, SESSION_KEY_LENGTH
+
+app = Flask(__name__)
+
+# APP CONFIG #
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Release only
+app.config['SQLALCHEMY_DATABASE_URI'] = "%s://%s:%s@%s/%s" % (DATABASE_PROVIDER, DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
+app.secret_key = urandom(SESSION_KEY_LENGTH)
+
+import core.views
