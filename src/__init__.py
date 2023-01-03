52227f68f2b1bc2f5e562f62a123f9d365aba04f
vincentprr <vincentprr@users.noreply.github.com>
2023-01-03 22:04:47 +0100

    feat: Reworking front.

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
-from core.app import app
-from core.database import create_tables, db
+from .core.app import app
+from .core.database import create_tables, db
 
-if __name__ == "__main__":
-    create_tables(db)
-    app.run(debug=True)
+create_tables(db)
+app.run(debug=True)
+    
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,12 +1,17 @@
 const HEADER_LOGO = document.getElementById("infos");
+const HEADER = document.querySelector("header");
+const HEADER_NAV = document.getElementById("navigation_menu");
+const BACKGROUND_COLOR = "linear-gradient(rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.4) 80%)";
+const POPUP = document.getElementById("popup");
 
 function currentScrollYPercentage()
 {
     return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
 }
 
-function dynamicScrollLogo(){
-    if (currentScrollYPercentage() > 50){
+function dynamicScroll(){
+    let scroll = currentScrollYPercentage();
+    if (scroll > 50){
         HEADER_LOGO.classList.remove("fade-in");
         HEADER_LOGO.classList.add("fade-out");
     }
@@ -14,10 +19,30 @@ function dynamicScrollLogo(){
         HEADER_LOGO.classList.remove("fade-out");
         HEADER_LOGO.classList.add("fade-in");
     }
+
+    if(document.documentElement.scrollTop + document.body.scrollTop > HEADER.offsetHeight - HEADER_NAV.offsetHeight)
+        HEADER_NAV.style.background = BACKGROUND_COLOR;
+    else
+        HEADER_NAV.style.background = "";
+}
+
+function popUp(){
+    if(POPUP != null){
+        // TO DO Change content if not the same source otherwise just print handle it with a tag
+        POPUP.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    }
+}
+
+function closePopUp(){
+    if(POPUP != null){
+        POPUP.style.display = "none";
+        document.body.style.overflow = "";
+    }
 }
 
 function dynamicalRenders(){
-    dynamicScrollLogo();
+    dynamicScroll();
 }
 
 window.onscroll = function(){
