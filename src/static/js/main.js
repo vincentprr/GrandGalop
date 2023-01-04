c3ea147fbb76b4391f1bcebcdfb63383a868a6c0
vincentprr <vincentprr@users.noreply.github.com>
2023-01-04 23:22:36 +0100

    feat: working on the popup

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -3,15 +3,24 @@ const HEADER = document.querySelector("header");
 const HEADER_NAV = document.getElementById("navigation_menu");
 const BACKGROUND_COLOR = "linear-gradient(rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.4) 80%)";
 const POPUP = document.getElementById("popup");
+const POPUP_CONTENT = document.getElementById("popup_content");
+
+const LOGIN_FORM = `
+
+`;
 
 function currentScrollYPercentage()
 {
-    return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let scrollPercent = scrollTop / (docHeight - winHeight);
+
+    return Math.round(scrollPercent * 100);
 }
 
 function dynamicScroll(){
-    let scroll = currentScrollYPercentage();
-    if (scroll > 50){
+    if (window.scrollY > 5 && window.scrollY > HEADER.offsetHeight / 3 - HEADER_NAV.offsetHeight){
         HEADER_LOGO.classList.remove("fade-in");
         HEADER_LOGO.classList.add("fade-out");
     }
@@ -26,10 +35,20 @@ function dynamicScroll(){
         HEADER_NAV.style.background = "";
 }
 
-function popUp(){
-    if(POPUP != null){
+function popUp(content, tagName, width = null){
+    if(POPUP != null && POPUP_CONTENT != null){
         // TO DO Change content if not the same source otherwise just print handle it with a tag
         POPUP.style.display = "flex";
+
+        if(POPUP.getAttribute("type") != tagName){
+            if(width != null)
+                POPUP_CONTENT.style.width = width;
+
+            POPUP.setAttribute("type", tagName);
+
+            POPUP_CONTENT.innerHTML = content;
+        }
+
         document.body.style.overflow = "hidden";
     }
 }
@@ -39,6 +58,10 @@ function closePopUp(){
         POPUP.style.display = "none";
         document.body.style.overflow = "";
     }
+}
+
+function loginPopUp(){
+    popUp(LOGIN_FORM, "login", "40%");
 }
 
 function dynamicalRenders(){
